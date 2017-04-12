0f01e64a806af1279cddc09c0c70cc486566749e
Eduard Generalov <eduard@generalov.net>
2017-04-12 04:42:10 +0300

    fix user add

--- add-user.sh
+++ add-user.sh
@@ -3,7 +3,7 @@
 echo "Adding new user"
         read -p "MySQL root password: " mysqlrootpasswd
 	read -p "Domain [example.com]: " domain
-	read -p "User [user@$domain]: " user
+	read -p "User [user][user@$domain]: " user
 	read -p "Password: " password
         echo "INSERT INTO \`users\` (\`email\`, \`password\`, \`quota\`, \`domain\`) VALUES ('$user@$domain', encrypt('$password'), '20971520', '$domain');" | mysql -p$mysqlrootpasswd mail
 
